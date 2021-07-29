f90e64bf2207537c453d92590a14f101e1270bde
noe <noe.reyes92@yahoo.com>
2021-07-29 10:52:22 -0500

    commit tree a61e02b878b669fece30065ad58a9602e07d7f9f

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,10 @@ class BirdsController < ApplicationController
     @birds = Bird.all
     render json: @birds
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+  render json: {id: bird.id, name: bird.name, species: bird.species } 
+  end
+
 end
